e33a57b3b7021393bfedbc9b2c9f8f243fd1ce85
Arun Kota <arunkumarkota@gmail.com>
2021-09-02 22:31:47 +0530

    Updates

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import pymongo
 from tabulate import tabulate
+from prettytable import PrettyTable
 
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
@@ -11,26 +12,39 @@ coll = db["Restaurants"]
 
 def selection1(cuisine,zipcode):
     query = {"FOODTYPE":cuisine, "ZIPCODE":zipcode}
-    print(f"Restaurant Name\t\t\t\tAddress\t\t\t\t\t\t\tRating\n")
+    myTable = PrettyTable(['RES_NAME', 'ADDRESS', 'RATING'])
     for x in coll.find(query).sort("RATING",-1).limit(5):
-        print(f"{x['RES_NAME']}\t\t{x['ADDRESS']}\t\t{x['RATING']}")
+        myTable.add_row([x['RES_NAME'],x['ADDRESS'], x['RATING']])
+    print(myTable)
 
 def selection2(address,rating):
     query_string= {'$regex' : address}
     condition = {'$gte' : rating}
+    myTable = PrettyTable(['RES_NAME', 'FOODTYPE'])
     for x in coll.find({'ADDRESS': query_string,"RATING" : condition}).sort("RATING",-1):
-        print(f"{x['RES_NAME']}   {x['FOODTYPE']}")
+        myTable.add_row([x['RES_NAME'],   x['FOODTYPE']])
+    print(myTable)
 
 def selection3(cusine):
-    count = 0
     agg_result = coll.aggregate([
         { '$match' : { 'FOODTYPE': cusine}},
         { '$group' : { '_id' : '$ZIPCODE', 'count' : { '$sum': 1}}},
-        {'$sort'   : {'count' : -1}}
+        { '$sort'   : {'count' : -1}}
     ])
+    myTable = PrettyTable(['ZIPCODE', 'Count'])
     for record in agg_result:
-        print(f' {record["_id"]}    {record["count"]}')
+        myTable.add_row([record["_id"], record["count"]])
+    print(myTable)
 
+def selection4():
+    agg_result = coll.aggregate([
+        { '$group' : { '_id' : '$FOODTYPE', 'count': {'$avg' : '$RATING'}}},
+        { '$sort'   : {'count' : -1}}
+    ])
+    myTable = PrettyTable(['Food Type', 'Average Rating'])
+    for record in agg_result:
+        myTable.add_row([record["_id"],record["count"]])
+    print(myTable)
 
 
 ################################### Input From the User:############################################################
